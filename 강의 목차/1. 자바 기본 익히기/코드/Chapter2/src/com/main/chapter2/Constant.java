00ea82deaca0580328649fae38fd02f5f7e29b71
HoJun Yang <didghwns0514@gmail.com>
2021-08-10 00:57:05 +0900

    doc,java(.md/.java): Add markdown, java files related to cp2 - 4
    body:
    Content
    - Java type converson; Implicit, Explicit
    - Java conversion flow; From small to big and big to small data types
    
    1) Markdown added regarding contents above
    2) Java added regarding contents above

---
+++ 강의 목차/1. 자바 기본 익히기/코드/Chapter2/src/com/main/chapter2/Constant.java
@@ -0,0 +1,25 @@
+package com.main.chapter2;
+
+public class Constant {
+    public static void main(String[] args) {
+        func1();
+        func2();
+    }
+
+    private static void func2() {
+        final int STUDENT_TOTAL_NUMBER = 30;
+        int num = 30;
+        if (num == STUDENT_TOTAL_NUMBER) {
+            System.out.println("Student number : " + STUDENT_TOTAL_NUMBER);
+        }
+    }
+
+    private static void func1() {
+        final int MAX_NUM = 100;
+        final float PI = 3.14F;
+
+        // PI = 3.13F; 는 오류가 나서 동작하지 않음
+
+    }
+
+}
