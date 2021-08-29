2a15a6659d5f69dcfd74a307d127669e26d6bb5f
HoJun Yang <didghwns0514@gmail.com>
2021-08-29 23:03:35 +0900

    doc(.md): Add file; Ch2 - 11
    Content
    - 2D arry in Java
    - Indexing of 2D array, and how to initialize them

---
+++ 강의 목차/코드/JavaPractice/src/com/main/Chapter2/array/TwoDimensionArray.java
@@ -0,0 +1,13 @@
+package com.main.Chapter2.array;
+
+public class TwoDimensionArray {
+
+    public static void main(String[] args) {
+        int[][] arr = new int[2][3];
+
+        System.out.println("arr.length = " + arr.length);
+
+        System.out.println("arr[0].length = " + arr[0].length);
+    }
+
+}
