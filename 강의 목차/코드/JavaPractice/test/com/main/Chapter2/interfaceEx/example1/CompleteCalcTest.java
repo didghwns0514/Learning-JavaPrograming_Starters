2fd31f44e57e1be820a0fe52697c0cd585dc7f29
HoJun Yang <didghwns0514@gmail.com>
2021-09-09 21:17:26 +0900

    doc,build,test(.md/.java): Add files; Ch2-19
    Content
    - Interface and implements to the class
    - Scope of Interface; It can be a abstract class w/o full overriding
    - Upcasting in Interface

--- 강의 목차/코드/JavaPractice/test/com/main/Chapter2/interfaceEx/example1/CompleteCalcTest.java
+++ 강의 목차/코드/JavaPractice/test/com/main/Chapter2/interfaceEx/example1/CompleteCalcTest.java
@@ -1,6 +1,17 @@
 package com.main.Chapter2.interfaceEx.example1;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CompleteCalcTest {
 
+    @Test
+    void completeCalcTest() {
 
+        int num1 = 2;
+        int num2 = 4;
+
+        Calc calc = new CompleteCalc();
+        Assertions.assertEquals(calc.add(num1, num2), 6);
+    }
 }
