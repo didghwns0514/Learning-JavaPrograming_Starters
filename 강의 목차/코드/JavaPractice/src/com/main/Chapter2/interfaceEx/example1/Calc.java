92cd68cc6b601ec25b8b22de3b890f7c8fa3c9b8
HoJun Yang <didghwns0514@gmail.com>
2021-09-14 23:01:08 +0900

    doc,build,test(.md/.java): Add files; Ch2-21(a)
    Content
    - Interface multiple usage;

--- 강의 목차/코드/JavaPractice/src/com/main/Chapter2/interfaceEx/example1/Calc.java
+++ 강의 목차/코드/JavaPractice/src/com/main/Chapter2/interfaceEx/example1/Calc.java
@@ -5,6 +5,16 @@ public interface Calc {
     double PI = 3.14; // 자동으로 public static final double PI = 3.14; 로 변환
     int ERROR = -999999999;
 
+    static int calculateTotalInArr(int[] arr) {
+
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+
+        return total;
+    }
+
     int add(int num1, int num2); // 자동으로 public abstract int add() 로 변환
 
     int subtract(int num1, int num2);
@@ -12,4 +22,9 @@ public interface Calc {
     int times(int num1, int num2);
 
     int divide(int num1, int num2);
+
+    default void description() { // 공통적으로 사용하는 메서드(원래는 제한자 default...)
+        System.out.println("정수 계산기를 구현합니다");
+
+    }
 }
--- 강의 목차/코드/JavaPractice/test/com/main/Chapter2/interfaceEx/example1/CompleteCalcTest.java
+++ 강의 목차/코드/JavaPractice/test/com/main/Chapter2/interfaceEx/example1/CompleteCalcTest.java
@@ -1,6 +1,7 @@
 package com.main.Chapter2.interfaceEx.example1;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CompleteCalcTest { // 클라이언트 부분(or 다른 서버코드를 가져다 쓰는 서버일 수도)
@@ -12,6 +13,19 @@ public class CompleteCalcTest { // 클라이언트 부분(or 다른 서버코드
         int num2 = 4;
 
         Calc calc = new CompleteCalc(); // 인터페이스 + 인터페이스 클라스 ; 클라이언트 코드 라고도 함(서버/기능 가져다 쓰기)
+
+        calc.description();
+
         Assertions.assertEquals(calc.add(num1, num2), 6);
+    }
+
+    @Test
+    @DisplayName("Test static method of interface")
+    void interfaceStaticTest() {
+        int[] arr = new int[]{
+                10, 15, 40
+        };
+
+        Assertions.assertEquals(Calc.calculateTotalInArr(arr), 65);
     }
 }
